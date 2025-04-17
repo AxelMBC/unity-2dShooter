82953a890d5739c665cc74243a9cc194e771ce29
AxelMBC <barrazaaxel2000@gmail.com>
2025-04-17 13:42:39 -0600

    audio agregado de forma correcta

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -1,24 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
     public float Speed;
+    public AudioClip Sound;
 
     private Rigidbody2D Rigidbody2D;
-    private Vector2 Direction;
+    private Vector3 Direction;
 
-    void Start()
+    private void Start()
     {
-        Rigidbody2D = GetComponent<Rigidbody2D>();    
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Camera.main.GetComponent<AudioSource>().PlayOneShot(Sound);
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         Rigidbody2D.linearVelocity = Direction * Speed;
     }
 
-    public void SetDirection(Vector2 direction)
+    public void SetDirection(Vector3 direction)
     {
         Direction = direction;
     }
@@ -26,5 +29,20 @@ public class BulletScript : MonoBehaviour
     public void DestroyBullet()
     {
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GruntScript grunt = other.GetComponent<GruntScript>();
+        JohnMovement john = other.GetComponent<JohnMovement>();
+        if (grunt != null)
+        {
+            grunt.Hit();
+        }
+        if (john != null)
+        {
+            john.Hit();
+        }
+        DestroyBullet();
     }
 }
--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,6 +7,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (John == null) return;
         Vector3 position = transform.position;
         position.x = John.transform.position.x;
         transform.position = position;
